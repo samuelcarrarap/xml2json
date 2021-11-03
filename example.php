6b64c538793cee9b54b981e37a32c291b0655a4c
Samuel Carrara <developer@samuelcarrara.com>
2021-11-03 13:51:08 -0300

    Removed error printing

--- example.php
+++ example.php
@@ -1,8 +1,4 @@
 <?php
-    ini_set('display_errors',1);
-    ini_set('display_startup_erros',1);
-    error_reporting(E_ALL);
-
     header('Content-Type: application/json; charset=utf-8');
 
     require_once('XML2Json.class.php');
